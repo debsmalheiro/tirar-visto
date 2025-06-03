d7c5b48d52310cca5132ccb180839dc3608a7693
Deborah Malheiro <malheirodev@gmail.com>
2025-06-03 20:05:39 -0300

    wip: add scroll in cards

--- src/templates/special-package-section.tsx
+++ src/templates/special-package-section.tsx
@@ -4,6 +4,7 @@ import Queue from "../assets/images/queue.jpg";
 import CheckIn from "../assets/images/check-in.jpg";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 import CardPackage from "../components/card-package";
+import { useRef } from "react";
 
 export const specialPackages = [
     {
@@ -45,27 +46,38 @@ export const specialPackages = [
 ];
 
 export default function SpecialPackageSection() {
+    const scrollRef = useRef<HTMLDivElement>(null);
+
+    const scrollLeft = () => {
+        scrollRef.current?.scrollBy({ left: -300, behavior: 'smooth' });
+    }
+
+    const scrollRight = () => {
+        scrollRef.current?.scrollBy({ left: 300, behavior: 'smooth' });
+    }
 
     return (
-        <section id="services" className="mt-10 py-10 px-4 lg:px-12">
+        <section id="services" className="mt-10 py-10 px-4 lg:px-12 overflow-hidden">
             <Button variant="secondary" className="py-2.5 px-5 md:mb-3">Pacotes</Button>
             <div className="mt-3 mb-14 flex justify-between">
                 <div>
-                    <h1 className="text-4xl md:text-5xl  font-medium">Confira nossos pacotes</h1>
+                    <h1 className="text-2xl md:text-3xl lg:text-4xl  font-medium">Confira nossos pacotes</h1>
                 </div>
                 <div className="flex gap-3">
-                    <Button className="p-2">
+                    <Button className="p-2" onClick={scrollLeft}>
                         <ArrowLeft className="text-white" />
                     </Button>
-                    <Button className="p-2">
+                    <Button className="p-2" onClick={scrollRight}>
                         <ArrowRight className="text-white" />
                     </Button>
                 </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div ref={scrollRef} className="flex gap-6 overflow-y-auto no-scrollbar">
                 {specialPackages.map(({ id, image, price, serviceTime, serviceType, title, tags }) => (
-                    <CardPackage key={id} id={id} image={image} price={price} serviceTime={serviceTime} serviceType={serviceType} tags={tags} title={title} />
+                    <div key={id} className="min-w-[350px] flex-shrink-0">
+                        <CardPackage id={id} image={image} price={price} serviceTime={serviceTime} serviceType={serviceType} tags={tags} title={title} />
+                    </div>
                 ))}
             </div>
         </section>
