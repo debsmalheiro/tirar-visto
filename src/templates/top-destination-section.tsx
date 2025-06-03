a981877d4947f0674fff1d220bfc0b89a4778e38
Deborah Malheiro <malheirodev@gmail.com>
2025-06-03 17:29:50 -0300

    wip: create top destination section

--- src/templates/top-destination-section.tsx
+++ src/templates/top-destination-section.tsx
@@ -1,21 +1,35 @@
 import Button from "../components/button";
+import Bali from "../assets/images/bali.jpg";
+import Paris from "../assets/images/paris.jpg";
+import NewYork from "../assets/images/ny.jpg";
+import Santorini from "../assets/images/santorini.jpg";
+import India from "../assets/images/india.jpg";
+import CidadeDoCabo from "../assets/images/cdc.jpg";
 
 export default function TopDestinationSection() {
     return (
-        <section>
-            <div>
-                <div>
-                    <Button variant="secondary" className="py-2.5 px-5 md:mb-3">Top Destinos</Button>
-                </div>
-                <h1>Destinos mais procurados</h1>
-                <p>Confira os destinos mais procurados por nossos clientes nos últimos meses, qual seria o seu novo destino?</p>
-                <div>
-                    <Button className="py-2.5 px-5 md:mb-3">Conheça mais</Button>
-                </div>
+        <section className="flex flex-col items-center justify-center">
+            <div className="w-full min-h-[423px] flex flex-col items-center justify-center text-center bg-[url('assets/images/bg-top-destination.png')] bg-cover bg-center bg-no-repeat">
+                <Button variant="secondary" className="py-2.5 px-5 md:mb-3">Top Destinos</Button>
+
+                <h1 className="my-3 md:my-4 text-4xl md:text-5xl font-medium">Destinos mais procurados</h1>
+                <p className="mb-8 md:mb-10 px-4 text-neutral-400">Confira os destinos mais procurados por nossos clientes nos últimos meses, qual seria o seu novo destino?</p>
+
+                <Button className="py-2.5 px-5 md:mb-3">Conheça mais</Button>
             </div>
 
             <div>
-                grid destinos
+                <div className="lg:flex lg:items-end gap-5">
+                    <img src={Bali} alt="Bali" className="mb-5 w-full h-60 object-cover rounded-2xl" />
+                    <img src={NewYork} alt="Nova York" className="mb-5 w-full h-60 md:h-72 object-cover rounded-2xl" />
+                    <img src={Santorini} alt="Santorini" className="mb-5 w-full h-60 object-cover rounded-2xl" />
+                </div>
+
+                <div className="lg:flex lg:items-start mt-5 gap-5">
+                    <img src={Paris} alt="Paris" className="mb-5 w-full h-60 object-cover rounded-2xl" />
+                    <img src={CidadeDoCabo} alt="Cidade do Cabo" className="mb-5 w-full h-60 md:h-72 object-cover rounded-2xl" />
+                    <img src={India} alt="Mumbai" className="mb-5 w-full h-60 object-cover rounded-2xl" />
+                </div>
             </div>
         </section>
     );
